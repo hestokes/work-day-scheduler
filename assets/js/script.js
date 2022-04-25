78e502dcba38e7699756f28c4785d4a46e03e663
Aaron Stokes <stokescomp01@gmail.com>
2022-04-25 12:31:14 -0600

    add save button functionality, test, works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // Today's day and date
 
-var todaysDate = moment().format("dddd, MMM Do YYYY");
-var currentTime = moment().format("HH:mm:ss")
+var todaysDate = moment().format("dddd, MMM Do YYYY", "");
+var currentTime = moment().format("HH:mm:ss");
 
-$("#currentDay").html(todaysDate +  currentTime);
+$("#currentDay").html(todaysDate + currentTime);
+
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    console.log("it worked");
+  });
+});
